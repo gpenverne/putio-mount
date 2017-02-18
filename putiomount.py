3ceb44206cbbcc47558d1f499f326d44db3c40d5
gpenverne <gpenverne@gmail.com>
2017-02-18 23:18:06 +0100

    update

--- putiomount.py
+++ putiomount.py
@@ -202,7 +202,7 @@ class Downloader:
         return resp.read()
 
 def main(mountpoint):
-    FUSE(PutioMount(), mountpoint, nothreads=False, foreground=False)
+    FUSE(PutioMount(), mountpoint, nothreads=False, foreground=False,**{'allow_other': True})
 
 if __name__ == '__main__':
     main(sys.argv[1])
