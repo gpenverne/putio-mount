0c8ca6a324b8ec3c239a0519c7264bb9da07d5f1
Grégoire <gpenverne@users.noreply.github.com>
2020-01-24 13:39:11 +0000

    Update for python3 (#11)
    * Update for python3
    
    * Now only for python3

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@
 
 from setuptools import setup, find_packages
 
-setup(name='PutioMount',
-      version='1.02',
-      description='Mount put.io as a local drive',
-      author='Gregoire Penverne',
-      keywords='put.io mount fuse',
-      packages=find_packages(),
-      license='MIT',
-      url='https://github.com/gpenverne/putio-mount',
-      install_requires=['putio.py', 'fusepy', 'requests', 'inotify', 'urllib3']
-     )
+setup(
+    name="PutioMount",
+    version="2",
+    description="Mount put.io as a local drive",
+    author="Gregoire Penverne",
+    keywords="put.io mount fuse",
+    packages=find_packages(),
+    license="MIT",
+    url="https://github.com/gpenverne/putio-mount",
+    install_requires=["putio.py", "fusepy", "requests", "inotify", "urllib3"],
+)
